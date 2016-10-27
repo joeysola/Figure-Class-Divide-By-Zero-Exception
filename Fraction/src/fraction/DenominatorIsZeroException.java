b7a9311013b148bd1c2e30451f762aab1413de78
joeysola <joeysolak@gmail.com>
2016-10-26 22:01:23 -0400

    Test 1

---
+++ Fraction/src/fraction/DenominatorIsZeroException.java
@@ -0,0 +1,39 @@
+/*
+ * Name: Joey Solak
+ * Date: 10/26/16
+ * Class: CIT-130
+ * Purpose Of The File: Add an exception for divide by zero to Fraction Class. 
+ */
+package fraction;
+
+/**
+ *
+ * @author Joey Solak
+ */
+public class DenominatorIsZeroException extends Exception {
+    
+    //Constructors
+    public DenominatorIsZeroException() {
+        super("Division by Zero!");
+                
+    }
+
+    public DenominatorIsZeroException(String message) {
+        super(message);
+    }
+    
+    
+    
+    
+
+
+    
+    
+    
+
+
+    
+    
+    
+    
+}
